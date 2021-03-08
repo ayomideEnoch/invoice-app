02df733c904971579282a5e5de816ff58dbd425b
Ayomide Ibosiola <ayomideibosiola100@gmail.com>
2021-03-08 09:27:30 +0100

    Iteration...

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,10 @@
   <section class="w-full h-full bg-gray-200 px-40 py-10">
     <div class="flex font-bold items-center py-2">
       <p class="capitalize">edit</p>
-      <p class="text-xl text-gray-700">#</p>
+      <p class="text-xl text-gray-700 ml-2">#</p>
       <p>XM9141</p>
     </div>
+    <div class="capitalize text-blue-900">bill from</div>
   </section>
 </template>
 
