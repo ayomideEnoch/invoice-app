2a0edec198ebfd0f37874700acbecc41c0a0d4cb
Ayomide Ibosiola <ayomideibosiola100@gmail.com>
2021-03-08 09:22:17 +0100

    Iteration...

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
 <template>
-  <section class="w-full h-full bg-gray-200 px-40 pt-10">a</section>
+  <section class="w-full h-full bg-gray-200 px-40 py-10">
+    <div class="flex font-bold items-center py-2">
+      <p class="capitalize">edit</p>
+      <p class="text-xl text-gray-700">#</p>
+      <p>XM9141</p>
+    </div>
+  </section>
 </template>
 
 <script>
