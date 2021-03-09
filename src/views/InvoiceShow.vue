e6ef600dddf1460e28acc3c182d33aafbb73d65d
Ayomide Ibosiola <ayomideibosiola100@gmail.com>
2021-03-09 17:53:35 +0100

    Iteration...

--- src/views/InvoiceShow.vue
+++ src/views/InvoiceShow.vue
@@ -38,13 +38,13 @@
     <section class="pt-6 bg-white shadow-md pl-12 pr-12 p-2 px-5 py-4 rounded-lg mb-5">
       <div class="flex justify-between">
         <div class="">
-          <div class="flex items-center">
-            <p class="text-xl text-gray-700">#</p>
-            <p>XM9141</p>
+          <div class="font-semibold text-lg">
+            <span class="text-gray-400">#</span>
+            <span>XM9141</span>
           </div>
           <p class="capitalize text-gray-700">graphic design</p>
         </div>
-        <div class="capitalize text-gray-700">
+        <div class="capitalize text-gray-700 text-right">
           <p class="">19 union terrace</p>
           <p class="">london</p>
           <p class="uppercase">e1 3ez</p>
@@ -53,13 +53,13 @@
       </div>
       <div class="flex justify-start pt-2 pb-8">
         <div class="capitalize">
-          <p class="text-gray-700">invoice data</p>
+          <p class="text-purple-900">invoice date</p>
           <p class="text-2xl pb-4">21 Aug 2021</p>
-          <p class="text-gray-700">payment due</p>
+          <p class="text-purple-900">payment due</p>
           <p class="text-2xl">20 Sept 2021</p>
         </div>
         <div class="capitalize px-30 ml-28">
-          <p class="text-gray-700">bill to</p>
+          <p class="text-purple-900">bill to</p>
           <p class="text-2xl">alex grim</p>
           <div class="text-gray-700">
             <p>84 church way</p>
@@ -69,35 +69,37 @@
           </div>
         </div>
         <div class="px-32">
-          <p class="text-gray-700">Sent to</p>
+          <p class="text-purple-900">Sent to</p>
           <p class="text-xl">alexgrim@mail.com</p>
         </div>
       </div>
-      <div class="flex justify-evenly bg-gray-100 shadow-2xl rounded-md">
+      <div class="flex justify-evenly bg-gray-100 shadow-xl rounded-md">
         <div class="capitalize py-10">
-          <p class="text-gray-700">item name</p>
+          <p class="text-black font-semibold">item name</p>
           <p class="py-10">banner design</p>
           <p class="">email design</p>
         </div>
         <div></div>
         <div class="uppercase py-10">
-          <p class="text-gray-700">qty.</p>
+          <p class="text-black font-semibold">qty.</p>
           <p class="py-10">1</p>
           <p>2</p>
         </div>
         <div class="capitalize py-10">
-          <p class="text-gray-700">price</p>
+          <p class="text-black font-semibold">price</p>
           <p class="py-10">#156.00</p>
           <p>#200.00</p>
         </div>
         <div class="capitalize py-10">
-          <p class="text-gray-700">total</p>
+          <p class="text-black font-semibold">total</p>
           <p class="py-10">#156.00</p>
           <p>#400.00</p>
         </div>
       </div>
-      <div class="flex justify-between bg-gray-400 px-28 shadow-2xl rounded-md mb-10">
-        <p class="capitalize flex items-center text-gray-700">amount due</p>
+      <div
+        class="flex justify-between bg-black px-28 shadow-xl rounded-md mb-10 text-white"
+      >
+        <p class="capitalize flex items-center text-sm">amount due</p>
         <p class="text-4xl py-4">#556.00</p>
       </div>
     </section>
