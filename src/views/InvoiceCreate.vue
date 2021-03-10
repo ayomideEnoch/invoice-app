87dd2640e0b8fbd088f4c702c3af733f26208922
Ayomide Ibosiola <ayomideibosiola100@gmail.com>
2021-03-10 16:29:02 +0100

    Iteration...

--- src/views/InvoiceCreate.vue
+++ src/views/InvoiceCreate.vue
@@ -1,75 +1,173 @@
 <template>
-  <section class="">
-    <div class="flex items-center py-2 font-semibold">
-      <p class="capitalize">edit</p>
-      <p class="text-xl text-gray-700 ml-2">#</p>
-      <p>XM9141</p>
+  <div class="">
+    <p class="font-semibold text-4xl mb-14">New Invoice</p>
+    <p class="text-purple-900 font-semibold mb-4">Bill From</p>
+
+    <div class="mb-4">
+      <label class="">
+        <span class="text-gray-400">Steet Address</span>
+        <input
+          class="rounded-md w-full border border-gray-300 font-semibold"
+          placeholder="Steet Address"
+          type="text"
+        />
+      </label>
     </div>
-    <div class="capitalize text-blue-900">bill from</div>
-    <div class="capitalize">
-      <p class="text-gray-700 mt-3 mb-1">street address</p>
-      <div class="bg-gray-50 py-2 px-6 rounded-md shadow-4xl">19 union terrace</div>
-    </div>
-    <div class="capitalize flex justify-evenly -ml-72">
-      <p class="text-gray-700 mt-3 mb-1">city</p>
-      <p class="text-gray-700 mt-3 mb-1">post code</p>
-      <p class="text-gray-700 mt-3 mb-1">country</p>
-    </div>
-    <div class="flex justify-evenly">
-      <p class="capitalize bg-gray-50 py-2 px-10 w-80 rounded-md shadow-4xl">london</p>
-      <p class="uppercase bg-gray-50 py-2 px-10 w-80 rounded-md shadow-4xl mx-12">
-        e1 3ez
-      </p>
-      <p class="capitalize bg-gray-50 py-2 px-10 w-80 rounded-md shadow-4xl">
-        united kingdom
-      </p>
-    </div>
-    <div class="capitalize text-blue-900 mt-4">bill to</div>
-    <div class="capitalize">
-      <p class="text-gray-700 mt-3 mb-1">client's name</p>
-      <div class="bg-gray-50 py-2 px-6 rounded-md shadow-4xl">alex grim</div>
-    </div>
-    <div class="capitalize">
-      <p class="text-gray-700 mt-3 mb-1">client's email</p>
-      <div class="bg-gray-50 py-2 px-6 rounded-md shadow-4xl">alexgrim@mail.com</div>
-      <div class="capitalize">
-        <p class="text-gray-700 mt-3 mb-1">street address</p>
-        <div class="bg-gray-50 py-2 px-6 rounded-md shadow-4xl">84 church way</div>
-      </div>
-      <div class="capitalize flex justify-evenly -ml-72">
-        <p class="text-gray-700 mt-3 mb-1">city</p>
-        <p class="text-gray-700 mt-3 mb-1">post code</p>
-        <p class="text-gray-700 mt-3 mb-1">country</p>
-      </div>
-      <div class="flex justify-evenly">
-        <p class="capitalize bg-gray-50 py-2 px-10 w-80 rounded-md shadow-4xl">
-          bradford
-        </p>
-        <p class="uppercase bg-gray-50 py-2 px-10 w-80 rounded-md shadow-4xl mx-12">
-          bd1 9db
-        </p>
-        <p class="capitalize bg-gray-50 py-2 px-10 w-80 rounded-md shadow-4xl">
-          united kingdom
-        </p>
+
+    <div class="flex mb-8">
+      <div class="w-1/3 mr-5">
+        <label class="">
+          <span class="text-gray-400">City</span>
+          <input
+            class="rounded-md w-full border border-gray-300 font-semibold"
+            placeholder="City"
+            type="text"
+          />
+        </label>
       </div>
 
-      <div class="capitalize flex justify-evenly -ml-72">
-        <p class="text-gray-700 mt-3 mb-1">invoice data</p>
-        <p class="text-gray-700 mt-3 mb-1">payment terms</p>
+      <div class="w-1/3 mr-5">
+        <label class="">
+          <span class="text-gray-400">Post Code</span>
+          <input
+            class="rounded-md w-full border border-gray-300 font-semibold"
+            placeholder="Post Code"
+            type="text"
+          />
+        </label>
       </div>
-      <div class="flex justify-evenly">
-        <p class="capitalize bg-gray-50 py-2 px-10 w-80 rounded-md shadow-4xl">
-          21 aug 2021
-        </p>
-        <div
-          class="flex justify-between items-center capitalize bg-gray-50 py-2 px-10 w-80 rounded-md shadow-4xl mx-12"
-        >
-          <p class="">net 30 days</p>
-          <i class="fas fa-angle-down cursor-pointer text-purple-400"></i>
-        </div>
+
+      <div class="w-1/3">
+        <label class="">
+          <span class="text-gray-400">Country</span>
+          <input
+            class="rounded-md w-full border border-gray-300 font-semibold"
+            placeholder="Country"
+            type="text"
+          />
+        </label>
       </div>
     </div>
-  </section>
+
+    <p class="text-purple-900 font-semibold mb-4">Bill To</p>
+
+    <div class="mb-4">
+      <label class="">
+        <span class="text-gray-400">Client's Name</span>
+        <input
+          class="rounded-md w-full border border-gray-300 font-semibold"
+          placeholder="Client's Name"
+          type="text"
+        />
+      </label>
+    </div>
+
+    <div class="mb-4">
+      <label class="">
+        <span class="text-gray-400">Client's Email</span>
+        <input
+          class="rounded-md w-full border border-gray-300 font-semibold"
+          placeholder="Client's Email"
+          type="email"
+        />
+      </label>
+    </div>
+
+    <div class="mb-4">
+      <label class="">
+        <span class="text-gray-400">Street Address</span>
+        <input
+          class="rounded-md w-full border border-gray-300 font-semibold"
+          placeholder="Street Address"
+          type="text"
+        />
+      </label>
+    </div>
+
+    <div class="flex mb-8">
+      <div class="w-1/3 mr-5">
+        <label class="">
+          <span class="text-gray-400">City</span>
+          <input
+            class="rounded-md w-full border border-gray-300 font-semibold"
+            placeholder="City"
+            type="text"
+          />
+        </label>
+      </div>
+
+      <div class="w-1/3 mr-5">
+        <label class="">
+          <span class="text-gray-400">Post Code</span>
+          <input
+            class="rounded-md w-full border border-gray-300 font-semibold"
+            placeholder="Post Code"
+            type="text"
+          />
+        </label>
+      </div>
+
+      <div class="w-1/3">
+        <label class="">
+          <span class="text-gray-400">Country</span>
+          <input
+            class="rounded-md w-full border border-gray-300 font-semibold"
+            placeholder="Country"
+            type="text"
+          />
+        </label>
+      </div>
+    </div>
+
+    <div class="flex mb-4">
+      <div class="w-1/2 mr-10">
+        <label class="">
+          <span class="text-gray-400">Invoice Date</span>
+          <input
+            class="rounded-md w-full border border-gray-300 font-semibold"
+            type="date"
+          />
+        </label>
+      </div>
+
+      <div class="w-1/2">
+        <label class="">
+          <span class="text-gray-400">Payment Terms</span>
+          <select class="rounded-md w-full border border-gray-300 font-semibold">
+            <option value="volvo">Net 30 Days</option>
+            <option value="saab">Net 60 Days</option>
+            <option value="mercedes">Net 90 Days</option>
+          </select>
+        </label>
+      </div>
+    </div>
+
+    <div class="mb-8">
+      <label class="">
+        <span class="text-gray-400">Project Description</span>
+        <input
+          class="rounded-md w-full border border-gray-300 font-semibold"
+          placeholder="Project Description"
+          type="text"
+        />
+      </label>
+    </div>
+
+    <div>
+      <p class="text-gray-500 text-xl font-semibold mb-4">Item List</p>
+    </div>
+
+    <div class="flex py-5 justify-end">
+      <button class="px-6 bg-gray-300 rounded-2xl p-2 ml-4 focus:outline-none">
+        <p class="text-black">Cancel</p>
+      </button>
+      <button
+        class="px-6 shadow-md bg-blue-700 rounded-2xl p-2 ml-4 focus:outline-none text-white"
+      >
+        <p>Save Changes</p>
+      </button>
+    </div>
+  </div>
 </template>
 
 <script>
--- src/views/InvoiceShow.vue
+++ src/views/InvoiceShow.vue
@@ -6,7 +6,7 @@
         class="flex justify-start items-center mb-6 focus:outline-none"
       >
         <i class="fa fa-angle-left mr-10 text-blue-600"></i>
-        <p>Go back</p>
+        <p class="font-bold">Go back</p>
       </button>
       <div
         class="flex justify-between items-center bg-white shadow-md p-2 pl-12 pr-12 px-5 py-4 rounded-lg mb-5"
@@ -24,7 +24,7 @@
         </div>
         <div class="flex py-3 text-white">
           <button
-            class="px-6 shadow-md bg-gray-300 rounded-2xl p-2 ml-4 capitalize focus:outline-none"
+            class="px-6 bg-gray-100 rounded-2xl p-2 ml-4 capitalize focus:outline-none"
           >
             <p class="text-black">edit</p>
           </button>
@@ -48,9 +48,9 @@
             <span class="text-gray-400">#</span>
             <span>XM9141</span>
           </div>
-          <p class="capitalize text-gray-700">graphic design</p>
+          <p class="capitalize text-gray-400">graphic design</p>
         </div>
-        <div class="capitalize text-gray-700 text-right">
+        <div class="capitalize text-gray-400 text-right">
           <p class="">19 union terrace</p>
           <p class="">london</p>
           <p class="uppercase">e1 3ez</p>
@@ -59,15 +59,15 @@
       </div>
       <div class="flex justify-start pt-2 pb-8">
         <div class="capitalize">
-          <p class="text-purple-900">invoice date</p>
-          <p class="text-2xl pb-4">21 Aug 2021</p>
-          <p class="text-purple-900">payment due</p>
-          <p class="text-2xl">20 Sept 2021</p>
+          <p class="text-gray-400">invoice date</p>
+          <p class="font-semibold text-lg">21 Aug 2021</p>
+          <p class="text-gray-400">payment due</p>
+          <p class="font-semibold text-lg">20 Sept 2021</p>
         </div>
         <div class="capitalize px-30 ml-28">
-          <p class="text-purple-900">bill to</p>
-          <p class="text-2xl">alex grim</p>
-          <div class="text-gray-700">
+          <p class="text-gray-400">bill to</p>
+          <p class="font-semibold text-lg">alex grim</p>
+          <div class="text-gray-400">
             <p>84 church way</p>
             <p>bradford</p>
             <p class="uppercase">bd1 9pb</p>
@@ -75,37 +75,39 @@
           </div>
         </div>
         <div class="px-32">
-          <p class="text-purple-900">Sent to</p>
-          <p class="text-xl">alexgrim@mail.com</p>
+          <p class="text-gray-400">Sent to</p>
+          <p class="font-semibold text-lg">alexgrim@mail.com</p>
         </div>
       </div>
-      <div class="flex justify-evenly bg-gray-100 shadow-xl rounded-md">
+      <div class="flex justify-evenly bg-gray-50 shadow-xl rounded-t-md">
         <div class="capitalize py-10">
-          <p class="text-black font-semibold">item name</p>
-          <p class="py-10">banner design</p>
-          <p class="">email design</p>
+          <p class="text-gray-400 font-semibold">item name</p>
+          <p class="py-10 font-semibold">banner design</p>
+          <p class="font-semibold">email design</p>
         </div>
         <div></div>
         <div class="uppercase py-10">
-          <p class="text-black font-semibold">qty.</p>
-          <p class="py-10">1</p>
-          <p>2</p>
+          <p class="text-gray-400 font-semibold">qty.</p>
+          <p class="py-10 font-semibold text-gray-400">1</p>
+          <p class="font-semibold text-gray-400">2</p>
         </div>
         <div class="capitalize py-10">
-          <p class="text-black font-semibold">price</p>
-          <p class="py-10">#156.00</p>
-          <p>#200.00</p>
+          <p class="text-gray-400 font-semibold">price</p>
+          <p class="py-10 text-gray-400 font-semibold">#156.00</p>
+          <p class="font-semibold text-gray-400">#200.00</p>
         </div>
         <div class="capitalize py-10">
-          <p class="text-black font-semibold">total</p>
-          <p class="py-10">#156.00</p>
-          <p>#400.00</p>
+          <p class="text-gray-400 font-semibold">total</p>
+          <p class="py-10 font-semibold">#156.00</p>
+          <p class="font-semibold">#400.00</p>
         </div>
       </div>
       <div
-        class="flex justify-between bg-black px-28 shadow-xl rounded-b-md mb-10 text-white"
+        class="flex justify-between bg-gray-700 px-28 shadow-xl rounded-b-md mb-10 text-white"
       >
-        <p class="capitalize flex items-center text-sm">amount due</p>
+        <p class="capitalize flex items-center text-sm text-gray-300 font-semibold">
+          amount due
+        </p>
         <p class="text-4xl py-4">#556.00</p>
       </div>
     </section>
